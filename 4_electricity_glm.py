ccfdef1862d2ece19e87d151e0ca50e3437d6303
fbchow <fannybchow@gmail.com>
2018-02-27 16:51:17 -0500

    start reading in regression componenets for mapper

--- 4_electricity_glm.py
+++ 4_electricity_glm.py
@@ -6,22 +6,35 @@ beta = summation(pop - pop_bar)(price - price_bar) / var(pop)*n
 
 run map reduce to compute pop, price, var(pop)
 have to run 2 jobs
+
+you can copy and paste the results of the previous job & use it in the next map-reduce
+popmean = 4334234
+priceman =343432
+pop var = 324234
+
+pipe output to a text file
+	python nfd.py > output.txt
+write output to a text file
+command line argument to copy small text file to every machine
+
 '''
 
+from mrjob.job import MRJob
 
 class MRPriceRegression(MRJob):
 
 
 	def mapper(self, _, line):
+		line = line.split(',')
+		price = float(line[1])
+		pop = float(line[2])		
+		yield 'key', ((pop - popmean)*(price-price_mean), 1) 
 
 
+	#def reducer(self, key, line):
 
 
-
-
-	def reducer(self, key, line):
-
-
-
+if __name__ == '__main__':
+	MRPriceRegression.run() 
 
 
