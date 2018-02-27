a788f544bc03d334577dd6266c49544aca1d229f
fbchow <fannybchow@gmail.com>
2018-02-27 16:19:29 -0500

    basic variance

--- 2_electricityvariance.py
+++ 2_electricityvariance.py
@@ -4,16 +4,26 @@ from mrjob.job import MRJob
 
 class MRElectricityVariance(MRJob):
 	def mapper(self, _, line):
-		element = line.split(',')
-
-		yield ('key', element)
-
-
-
-	#def reducer(self, key, values):
+		line = line.split(',')
+		state = line[0]	
+		price = line[1]
+		yield 'key', (price, price*price)
 
 
-		# yield
+	def reducer(self, key, values):
+		values = list(values)	
+		ex_x = sum(values) / len(values)
+		
+		sum = 0
+		count = 0
+		sum_square = 0
+
+		for val in values:
+			sum += val[0]
+			sum_square += val[1]
+			count += 1
+		yield "var", (sum_square/count) - (sum*sum/(count*count))		
+
 
 if __name__ == '__main__':
 	MRElectricityVariance.run()
