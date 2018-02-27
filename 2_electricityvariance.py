33cbd5b182303f3f3b52e3e511e8ca6f4d120e18
fbchow <fannybchow@gmail.com>
2018-02-27 16:29:42 -0500

    added combiner to optimize code

--- 2_electricityvariance.py
+++ 2_electricityvariance.py
@@ -7,12 +7,25 @@ class MRElectricityVariance(MRJob):
 		line = line.split(',')
 		state = line[0]	
 		price = line[1]
-		yield 'key', (price, price*price)
+		yield 'key', (price, price*price, 1) # add 1 to indicate entity for combiner to count
 
+	def combiner(self, key, some_vals):
+	'''
+	only takes in some of the values associated with the key -- unlike reducer
+	'''
+	sum = 0
+	count = 0
+	sum_square = 0
+	for val in some_vals:
+		sum += val[0]
+		sum_square += val[1]
+		count += val[2]
+	yield "key", (sum, sum_square, count)	
+	
 
 	def reducer(self, key, values):
 		values = list(values)	
-		ex_x = sum(values) / len(values)
+#		ex_x = sum(values) / len(values)
 		
 		sum = 0
 		count = 0
