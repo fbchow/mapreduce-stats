22822e5d1b59ce577776930f9171b23f20463913
fbchow <fannybchow@gmail.com>
2018-03-20 22:41:14 -0400

    finished simple OLS for predicting population, started OLS for predicting problem #4

--- 3_population_glm.py
+++ 3_population_glm.py
@@ -12,22 +12,21 @@ alpha = y_hat - beta
 
 '''
 
-from mrjob.job import mrjob
+from mrjob.job import MRJob
 
-class MRRegression():
+# outuputs computed from previous problems
+pop_mean = 4876487.7058823528
+area_var = 9006334405.7747020721 
+area_mean = 74261.098 
 
-    # outuputs computed from previous problems
-    pop_mean = 4876487.7058823528
-    pop_var = 
-    area_mean = 74261.098 
+class MRRegression(MRJob):
 
     def mapper(self, _, line):
         line = line.split(',')
-        area = line[2]
-        pop = line[3]
-        xy = (area - area_mean)(pop - pop_mean)
-
-       yield 'key', (xy,1) 
+        area = float(line[3])
+        pop = float(line[4])
+        xy = (area - area_mean)*(pop - pop_mean)
+        yield 'key', (xy, 1) 
     
     def combiner(self, key, some_vals):
         count = 0
@@ -38,7 +37,7 @@ class MRRegression():
             s_xy += val[0]
         yield 'key', (s_xy, count)
 
-    def reduce(self, _, values):
+    def reducer(self, _, values):
         count = 0
         s_xy = 0
         
@@ -46,11 +45,11 @@ class MRRegression():
             s_xy = val[0]
             count = val[1]
 
-        beta = s_xy/(n*area_var)
+        beta = s_xy/(count*area_var)
         alpha = pop_mean - (beta*area_mean) 
             
         yield 'slope', beta
         yield 'intercept', alpha
 
-
-
+if __name__ == '__main__':
+	MRRegression.run()
